0ce1cf4018de901ae882badec6fee99bf50429a2
MridulaRT <49071305+MridulaRT@users.noreply.github.com>
2019-04-24 01:36:27 +0800

    Update 1D_thymio.py

--- 1D_thymio.py
+++ 1D_thymio.py
@@ -20,35 +20,30 @@ config = {
 # the database and also retrieve data from the database.
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-water_list = db.child("thymio").get()
 
-robot = ThymioReal()  # create a robot object
-
-no_movements = True
-
-while no_movements:
+while True:
     # Check the value of water_list in the database at an interval of 0.5
     # seconds. Continue checking as long as the water_list is not in the
     # database (ie. it is None). If water_list is a valid list, the program
     # exits the while loop and controls the robot to move forward to close the
     # window
-    check = str(water_list.val())
-    if check == "None":
+    robot = ThymioReal()  # create a robot object
+    water_list = db.child("thymio").get()
+    water_value = str(water_list.val())
+    
+    if water_value == "None":
         time.sleep(0.5)
-        return no_movements
+    
     else:
-        break
+        if water_value == "Yes":
+            robot.wheels(100, 100)
 
-# code to control the robot
-for water_value in water_list:
-    if water_value == "Yes":  ###
-        robot.wheels(100, 100)
-        robot.sleep(1)    ###value inside sleep is based on actual set size###
-
-for water_value in water_list:
-    if water_value == "No":  ###
-        robot.wheels(-100, -100)
-        robot.sleep(1)
+        elif water_value == "No":
+            robot.wheels(-100, -100)
+        
+        robot.sleep(3)
+        robot.quit()
+        db.child("thymio").set("None")
 
 # 'up' movement => robot.wheels(100, 100)
 # 'left' movement => robot.wheels(-100, 100)
