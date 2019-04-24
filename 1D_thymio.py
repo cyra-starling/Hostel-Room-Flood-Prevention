3af973cb8fa5099762bdcfd06ea86349dfdaf133
MridulaRT <49071305+MridulaRT@users.noreply.github.com>
2019-04-24 18:48:40 +0800

    Update 1D_thymio.py

--- 1D_thymio.py
+++ 1D_thymio.py
@@ -2,49 +2,48 @@ from pythymiodw import *
 from time import sleep
 from libdw import pyrebase
 
-
-url = 'https://dw-project-d22fe.firebaseio.com/'
 # URL to Firebase database
+url = 'https://dw-project-d22fe.firebaseio.com/'
+# Unique token used for authentication
 apikey = 'AIzaSyCwGVwAcf2XLeRtMd1sbgt3NlNxPVmIc0E'
-# unique token used for authentication
 
 config = {
     "apiKey": apikey,
     "databaseURL": url,
 }
 
-# Create a firebase object by specifying the URL of the database
-#and its secret token.
-# The firebase object has functions put and get, that
-#allows user to put data onto
-# the database and also retrieve data from the database.
+# Create a firebase object by specifying the URL of the database and its secret
+# token.
+# The firebase object has functions put and get, that allows user to put data
+# onto the database and also retrieve data from the database.
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 while True:
-    # Check the value of water_list in the database at an interval of 0.5
-    # seconds. Continue checking as long as the water_list is not in the
-    # database (ie. it is None). If water_list is a valid list, the program
-    # exits the while loop and controls the robot to move forward to close the
-    # window
-    robot = ThymioReal()  # create a robot object
+    # Create a robot object
+    robot = ThymioReal()
+
+    # Check the value of database child 'thymio' in the database at an interval
+    # of 0.5 seconds. Continue checking as long as the value is 'None'.
     water_list = db.child("thymio").get()
     water_value = str(water_list.val())
-    
+
     if water_value == "None":
         time.sleep(0.5)
-    
+
     else:
+        # If value is 'Yes', the program moves the robot to close the window.
+        # 'forward' movement => robot.wheels(100, 100)
         if water_value == "Yes":
             robot.wheels(100, 100)
-
+        # If 'No', the robot moves to open the window.
+        # 'backward' movement => robot.wheels(-100, -100)
         elif water_value == "No":
             robot.wheels(-100, -100)
-        
+
+        # The robot moves for 3 seconds and is terminated.
         robot.sleep(3)
         robot.quit()
-        db.child("thymio").set("None")
 
-# 'up' movement => robot.wheels(100, 100)
-# 'left' movement => robot.wheels(-100, 100)
-# 'right' movement => robot.wheels(100, -100)
+        # Reset the value of the database child to 'None'
+        db.child("thymio").set("None")
